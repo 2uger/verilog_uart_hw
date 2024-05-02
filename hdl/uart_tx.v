4e47355562e00dba955f17920f39ab3a1ee30453
Oleg <oleg.ignatenko12@gmail.com>
2024-05-02 15:32:54 +0300

    Start rewriting.

--- hdl/uart_tx.v
+++ hdl/uart_tx.v
@@ -15,7 +15,8 @@ module uart_tx #(
     /* Count time between bits. */
     reg [$clog2(CLKS_PER_BIT)-1:0] timer_cnt;
 
-    reg [2:0] fsm_state = 3'b0;
+    reg [2:0] state = 3'b0;
+    reg [2:0] next_state = 3'b0;
     localparam IDLE = 1;
     localparam START = 2;
     localparam DATA = 3;
@@ -26,7 +27,8 @@ module uart_tx #(
 
     always @(posedge clk) begin
         if (!resetn) begin
-            fsm_state <= IDLE;
+            state <= IDLE;
+            next_state <= IDLE;
             data      <= 0;
             bit_idx   <= 0;
             timer_cnt <= CLKS_PER_BIT;
@@ -34,49 +36,53 @@ module uart_tx #(
             tx_o   <= 1;
             busy_o <= 0;
         end else begin
-            case (fsm_state)
-                IDLE: begin
-                    tx_o      <= 1;
-                    bit_idx   <= 0;
-                    timer_cnt <= CLKS_PER_BIT;
-                    if (e_i) begin
-                        busy_o    <= 1'b1;
-                        data      <= d_i;
-                        fsm_state <= START;
-                    end
-                end
-                START: begin
-                    /* Start bit. */
-                    tx_o <= 0;
-                    /* Wait till start bit finish. */
-                    timer_cnt <= timer_cnt - 1;
-                    if (timer_cnt == 0) begin
-                        timer_cnt <= CLKS_PER_BIT;
-                        fsm_state <= DATA;
-                    end
-                end
-                DATA: begin
-                    tx_o <= data[bit_idx];
-                    timer_cnt <= timer_cnt - 1;
-                    if (timer_cnt == 0) begin
-                        timer_cnt <= CLKS_PER_BIT;
-                        fsm_state <= bit_idx < 7 ? DATA : STOP;
-                        bit_idx   <= bit_idx < 7 ? bit_idx + 1 : 0;
-                    end
-                end
-                STOP: begin
-                    tx_o <= 1;
-                    /* Wait till stop bit finish. */
-                    timer_cnt <= timer_cnt - 1;
-                    if (timer_cnt == 0) begin
-                        fsm_state <= IDLE;
-                        busy_o    <= 0;
-                    end
-                end
-                default:
-                    fsm_state <= IDLE;
-            endcase
+            state <= next_state;
         end
+    end
+
+    always @* begin
+        case (state)
+            IDLE: begin
+                tx_o      = 1;
+                bit_idx   = 0;
+                timer_cnt = CLKS_PER_BIT;
+                if (e_i) begin
+                    busy_o    = 1'b1;
+                    data      = d_i;
+                    next_state <= START;
+                end
+            end
+            START: begin
+                /* Start bit. */
+                tx_o <= 0;
+                /* Wait till start bit finish. */
+                timer_cnt <= timer_cnt - 1;
+                if (timer_cnt == 0) begin
+                    timer_cnt <= CLKS_PER_BIT;
+                    state <= DATA;
+                end
+            end
+            DATA: begin
+                tx_o <= data[bit_idx];
+                timer_cnt <= timer_cnt - 1;
+                if (timer_cnt == 0) begin
+                    timer_cnt <= CLKS_PER_BIT;
+                    state <= bit_idx < 7 ? DATA : STOP;
+                    bit_idx   <= bit_idx < 7 ? bit_idx + 1 : 0;
+                end
+            end
+            STOP: begin
+                tx_o <= 1;
+                /* Wait till stop bit finish. */
+                timer_cnt <= timer_cnt - 1;
+                if (timer_cnt == 0) begin
+                    state <= IDLE;
+                    busy_o    <= 0;
+                end
+            end
+            default:
+                state <= IDLE;
+        endcase
     end
 endmodule
 
