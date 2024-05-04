3d42a2bc660e919386c7b45bfe5156e85a78b998
Oleg <oleg.ignatenko12@gmail.com>
2024-05-04 15:12:18 +0300

    Update test.

--- tb/test_uart_loopback.py
+++ tb/test_uart_loopback.py
@@ -3,43 +3,6 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge
 
 CLKS_PER_BIT = 120
-TEST_VALUE = 0b00001011
-
-@cocotb.test()
-async def uart_tx_test(dut):
-    global CLKS_PER_BIT
-    CLKS_PER_BIT = dut.CLKS_PER_BIT.value
-    cocotb.start_soon(Clock(dut.clk, 2, units='ns').start())
-
-    await reset_dut(dut)
-
-    for _ in range(5):
-        await RisingEdge(dut.clk)
-
-    # Start bit
-    dut.rx_i.value = 0
-    await wait_one_bit(dut)
-
-    # Data
-    for b in (bin(ord('H'))[2:])[::-1]:
-        dut.rx_i.value = int(b)
-        await wait_one_bit(dut)
-
-    # Stop bit
-    dut.rx_i.value = 1
-    await wait_one_bit(dut)
-
-    # Wait for receiver
-    while not dut.rx_done_o.value:
-        await RisingEdge(dut.clk)
-
-    assert dut.uart_rx.d_o.value == ord('H')
-
-    while not dut.tx_busy.value:
-        await RisingEdge(dut.clk)
-
-    while dut.tx_busy.value:
-        await RisingEdge(dut.clk)
 
 async def wait_one_bit(dut):
     clks = CLKS_PER_BIT
@@ -54,4 +17,38 @@ async def reset_dut(dut):
     dut.resetn.value = 1
     await RisingEdge(dut.clk)
     await RisingEdge(dut.clk)
+
+@cocotb.test()
+async def uart_tx_test(dut):
+    global CLKS_PER_BIT
+    CLKS_PER_BIT = dut.CLKS_PER_BIT.value
+    cocotb.start_soon(Clock(dut.clk, 2, units='ns').start())
+
+    await reset_dut(dut)
+    
+    for d in "Hello":
+        # Start bit
+        dut.rx_i.value = 0
+        await wait_one_bit(dut)
+
+        # Data
+        for b in (bin(ord(d))[2:])[::-1]:
+            dut.rx_i.value = int(b)
+            await wait_one_bit(dut)
+
+        # Stop bit
+        dut.rx_i.value = 1
+        await wait_one_bit(dut)
+
+        # Wait for receiver
+        while not dut.rx_done_o.value:
+            await RisingEdge(dut.clk)
+
+        assert dut.uart_rx.d_o.value == ord(d)
+    
+    while not dut.tx_busy.value:
+        await RisingEdge(dut.clk)
+
+    while dut.tx_busy.value:
+        await RisingEdge(dut.clk)
 
